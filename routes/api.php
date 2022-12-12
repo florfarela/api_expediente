efc59c219c8c724ee1d305ba4e7e697792922f68
ghfescalon <francisco.escalon10@hotmail.com>
2022-12-12 10:42:05 -0600

    adding roles routes in api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;  //Agregamos el controlador para el paciente
-use App\Http\Controllers\LoginController; 
+use App\Http\Controllers\LoginController; //Adding the login controller
+use App\Http\Controllers\RoleController; //Adding the role Controller
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +32,11 @@ Route::delete('/patientDelete/{id}',[PatientController::class,'destroy']); //Eli
 //Login
 Route::get('/Login', [LoginController::class, 'index']);
 Route::post('/LoginStore', [LoginController::class, 'store']);
+
+//Role routes
+Route::get('/RoleIndex', [RoleController::class,'index']); //Showing all data about role
+Route::post('/RoleStore', [RoleController::class,'store']); //creating a new role
+Route::get('/RoleShow/{id}', [RoleController::class,'show']); //showing a role
+Route::put('/RoleUpdate/{id}', [RoleController::class,'update']); //updating a role
+Route::delete('/RoleDestroy/{id}', [RoleController::class,'destroy']); //deleting a role
+//Keep you in geek mode my friend!
